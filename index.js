ebe2ec62d6f4913fb0c8ef3dcbbb2acf15914254
Eduardo Fortuna <eduardofortuna99@gmail.com>
2020-12-03 21:02:28 -0600

    Reiniciamos el acordeon

--- index.js
+++ index.js
@@ -1,46 +1,95 @@
 //Declarando los listeners
-document.body.addEventListener("click", elementoClick);
+const escuchandoElBody = document.body.addEventListener("click", elementoClick);
+
+
+
 
 //Declaramos las funciones
-    //Funcion que detecta que elemento se dio click y se llama a la funcion para abrir o cerrar
-function elementoClick (e){
-    //Previniendo un efecto default
+
+function elementoClick(e) {
     e.preventDefault();
     //Definiendo los elementos que pueden ser clickados
-    const cardTittleClicked = e.target.classList.contains('card-tittle')
-    const cardTittleArrowClicked = e.target.classList.contains('card-tittle-arrow')
-    const cardTittleTextClicked = e.target.classList.contains('card-tittle-text')
-    //Condicional que verifica si los elementos clickados son padres
+    const cardTittleClicked = e.target.classList.contains('card-tittle');
+    const cardTittleArrowClicked = e.target.classList.contains('card-tittle-arrow');
+    const cardTittleTextClicked = e.target.classList.contains('card-tittle-text');
+
+
     if (cardTittleClicked) {
-        //Se selecciona el hijo que será el que se activará
-        const elemento = e.target.childNodes[5];
-        //Se verifica si el elemento hijo esta activo o no
-        const elementoActivo = elemento.classList.contains('active')
-        //Si el elemento esta activo, al dar click se ocultara, sino se hará lo contrario
-        if (elementoActivo){
-            cerrarAcordeon(elemento);
-        } else {
-            abrirAcordeon(elemento);
-        }
-   }
-   //Condicional que verifica si el elemento cickado es hermano
-   else if(cardTittleTextClicked || cardTittleArrowClicked){
-        const elementoHermano = e.target.parentElement.childNodes[5]
-        const elementoHermanoActivo = elementoHermano.classList.contains('active')
-        if (elementoHermanoActivo){
-            cerrarAcordeon(elementoHermano);
-        } else{
-            abrirAcordeon(elementoHermano);
+        seClickoEnElPadre(e);
+    } else if (cardTittleTextClicked || cardTittleArrowClicked) {
+        seClickoEnHermano(e);
+    }
+}
+
+//Funciones
+
+    //Reinicia los acordeones al dar click, se cierra el que estaba abierto si da click en otro
+function reiniciaTodosLosAcordeones () {
+    //Selecciona todos los titulos de acordeones y los convierte a un array
+    let tittle = document.querySelectorAll('.card-tittle-text');
+    tittle = [...tittle];
+
+    /*Recorre cada titulo y luego recorre cada clase dentro de los titulos
+    posterior a eso verifica si cuentan con la clase tittle active y si es así
+    remueve la clase activa del elemento y de sus hermanos || Es de nivel EXPONENCIAL, se debe buscar OPTIMIZAR
+     */
+    for (let i = 0; i<tittle.length; i++){
+        for (let j=0; j<tittle[i].classList.length; j++){
+            if (tittle[i].classList[j] == 'tittle-active'){
+                tittle[i].classList.remove('tittle-active');
+                tittle[i].parentElement.childNodes[3].classList.remove('active-Arrow');
+                tittle[i].parentElement.childNodes[5].classList.remove('active');
+            }
         }
     }
 }
 
-function abrirAcordeon (elemento){
+function seClickoEnElPadre(e) {
+    const elemento = e.target.childNodes[5];
+    const arrow = e.target.childNodes[3];
+    const tittle = e.target.childNodes[1];
+    const elementoActivo = elemento.classList.contains('active');
+    if (elementoActivo) {
+        reiniciaTodosLosAcordeones();
+        cerrarAcordeon(elemento, arrow, tittle);
+    } else {
+        reiniciaTodosLosAcordeones();
+        abrirAcordeon(elemento, arrow, tittle);
+    }
+}
+
+function seClickoEnHermano(e) {
+    const elementoHermano = e.target.parentElement.childNodes[5];
+    const arrowHermana = e.target.parentElement.childNodes[3];
+    const padreHermano = e.target.parentElement.childNodes[1];
+    const elementoHermanoActivo = elementoHermano.classList.contains('active');
+    if (elementoHermanoActivo) {
+        reiniciaTodosLosAcordeones();
+        cerrarAcordeon(elementoHermano, arrowHermana, padreHermano);
+    } else {
+        reiniciaTodosLosAcordeones();
+        abrirAcordeon(elementoHermano, arrowHermana, padreHermano);
+    }
+}
+
+function abrirAcordeon(elemento, arrow, tittle) {
+    //Añadimos el Active al Div oculto
     elemento.classList.add('active');
+    //Rotamos la arrow
+    arrow.classList.add('active-Arrow');
+    //Le damos negrita al title
+    tittle.classList.add('tittle-active');
 }
 
 
-function cerrarAcordeon (elemento){
-    elemento.classList.remove('active')
+function cerrarAcordeon(elemento, arrow, tittle) {
+    elemento.classList.remove('active');
+    arrow.classList.remove('active-Arrow');
+    //Le quitamos negrita al title
+    tittle.classList.remove('tittle-active');
 }
+
+
+
+
 
